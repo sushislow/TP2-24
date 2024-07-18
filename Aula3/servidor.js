43fff449a94e32919587f72fbf493ab04fb26ee1
sushislow <eduardosf2307@gmail.com>
2024-07-17 21:38:19 -0300

    17/07

--- Aula3/servidor.js
+++ Aula3/servidor.js
@@ -46,8 +46,6 @@ app.get('/div',(request,response) =>{
             console.log(`${resp}`)
             })
 
-
-
 const port = 8080
 app.listen(port)
 console.log(`O servidor está rodando na porta: ${port}`)
